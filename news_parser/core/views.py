6d7af6b636d9b04e4e674ba5e8693ae7d4d8909a
se1f4 <58149320+se1f4@users.noreply.github.com>
2022-05-02 22:04:12 +0300

    Added the ability to view the article

--- news_parser/core/views.py
+++ news_parser/core/views.py
@@ -8,32 +8,40 @@ def get_html_content(request, slug=None):
     if slug == None:
         news = request.GET.get('news')
         news = news.replace(" ", "+")
-        html_content = session.get(f'https://tsn.ua/ru/search?query=+{news}').text
+        html_content = session.get(f'https://zaxid.net/search/search.do?search={news}').text
     else:
-        category = slug.split('-')[0]
-        slug_without_category = slug.split('-', 1)[1]
-        html_content = session.get(f'https://tsn.ua/ru/{category}/{slug_without_category}').text
+        html_content = session.get(f'https://zaxid.net/{slug}').text
 
     return html_content
 
 #fetch the data of the article from the news site
 def collect_all_articles(soup):
     articels = []
-    soup = soup.find("div", attrs={"class": "c-feed-log--y c-feed-log--y-start"}, id="loadmore")
-    for soup_article in soup.find_all("article"):
+    soup = soup.find("ul", attrs={"class": "list search_list"})
+    for soup_article in soup.find_all("li"):
         article = dict()
-        article['time'] = soup_article.find("time").text
-        article['header_article'] = soup_article.find("a", attrs={"class": "c-card__link"}).text
-        article['img'] = soup_article.find("img")['data-src']
-
-        link_to_article = soup_article.find("a", attrs={"class": "c-card__link"})['href']
-        slug = link_to_article.split('ru/')[1]
-        slug_combined_category = slug.replace("/", "-")
-        article['slug'] = slug_combined_category
+        article['type'] = soup_article.find("span", attrs={"class": "type"}).text
+        article['category'] = soup_article.find("a", attrs={"class": "category"}).text
+        article['date'] = soup_article.find("span", attrs={"class": "date"}).text
+        article['time'] = soup_article.find("span", attrs={"class": "time"}).text
+        article['title'] = soup_article.find_next("div", attrs={"class": "title"}).text
+        article['description'] = soup_article.find("div", attrs={"class": "desc"}).text
+        article['slug'] = soup_article.find("div", attrs={"class": "title"}).find('a')['href'].split('https://zaxid.net/')[1]
 
         articels.append(article)
 
     return articels
+
+#fetch the content of the article
+def collect_data_of_article(soup):
+    article = dict()
+    article['img'] = soup.find("span", attrs={"class": "lazyload-holder"}).find('img')['srcset']
+    article['category'] = soup.find("a", attrs={"class": "category"}).text
+    article['time'] = soup.find("time", attrs={"class": "date"}).text
+    article['title'] = soup.find("h1", attrs={"class": "title"}, id="newsName").text
+    article['article'] = soup.find("div", attrs={"class": "newsSummary"}, id="newsSummary")
+
+    return article
 
 def news_search(request):
     articles = None
@@ -45,8 +53,8 @@ def news_search(request):
     return render(request, 'core/news_search.html', {'articels': articles})
 
 def article(request, slug):
-    article = dict()
     html_content = get_html_content(request, slug)
-    article['html_content'] = html_content
+    soup = BeautifulSoup(html_content, 'html.parser')
+    article = collect_data_of_article(soup)
 
     return render(request, 'core/article.html', {'article': article})
