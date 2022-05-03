9942baa8142a79b98068ff331b3088f1a6f47110
se1f4 <58149320+se1f4@users.noreply.github.com>
2022-05-03 16:46:54 +0300

    Сollected and filtered the entire text of the article

--- news_parser/core/views.py
+++ news_parser/core/views.py
@@ -1,6 +1,6 @@
 import requests
 from django.shortcuts import render
-from bs4 import BeautifulSoup, SoupStrainer
+from bs4 import BeautifulSoup, Comment
 
 
 def get_html_content(request, slug=None):
@@ -8,7 +8,7 @@ def get_html_content(request, slug=None):
     if slug == None:
         news = request.GET.get('news')
         news = news.replace(" ", "+")
-        html_content = session.get(f'https://zaxid.net/search/search.do?search={news}').text
+        html_content = session.get(f'https://zaxid.net/search/search.do?searchValue={news}').text
     else:
         html_content = session.get(f'https://zaxid.net/{slug}').text
 
@@ -32,6 +32,13 @@ def collect_all_articles(soup):
 
     return articels
 
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
 #fetch the content of the article
 def collect_data_of_article(soup):
     article = dict()
@@ -39,7 +46,13 @@ def collect_data_of_article(soup):
     article['category'] = soup.find("a", attrs={"class": "category"}).text
     article['time'] = soup.find("time", attrs={"class": "date"}).text
     article['title'] = soup.find("h1", attrs={"class": "title"}, id="newsName").text
-    article['article'] = soup.find("div", attrs={"class": "newsSummary"}, id="newsSummary")
+
+    soup_text = soup.find("div", attrs={"class": "newsSummary"}, id="newsSummary")
+    unwanted = soup_text.find("div", attrs={"class": "subscribe-wrap"})
+    unwanted.extract()
+    texts = soup_text.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)
+    article['article'] = u" ".join(t.strip() for t in visible_texts)
 
     return article
 
